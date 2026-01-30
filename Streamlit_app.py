058b58735e17017de62d8f23e72a86c53b59583c
KaviyanMK <145440245+KaviyanMK@users.noreply.github.com>
2026-01-30 20:52:33 +0530

    Display fruit options in Streamlit app
    Added functionality to display fruit options in a dataframe.

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -23,6 +23,13 @@ fruit_names = [row["FRUIT_NAME"] for row in fruit_data]
 search_lookup = {
     row["FRUIT_NAME"]: row["SEARCH_ON"] for row in fruit_data
 }
+my_dataframe = session.table("smoothies.public.fruit_options") \
+    .select(col("FRUIT_NAME"), col("SEARCH_ON"))
+
+pd_df = my_dataframe.to_pandas()
+
+st.dataframe(pd_df)
+st.stop()
 
 ingredients_list = st.multiselect(
     "Choose up to 5 ingredients:",
